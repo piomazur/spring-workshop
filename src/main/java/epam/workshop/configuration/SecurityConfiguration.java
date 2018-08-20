b4499390918d1d1fd8b8c2c0de44ce0cd93e908e
piotr <pimazur@gmail.com>
2018-08-20 10:25:05 +0200

    default

--- src/main/java/epam/workshop/configuration/SecurityConfiguration.java
+++ src/main/java/epam/workshop/configuration/SecurityConfiguration.java
@@ -3,7 +3,6 @@ package epam.workshop.configuration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,7 +11,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Slf4j
-@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
