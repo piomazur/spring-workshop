e23a4008d433a165ec7a9e7f25d02599171ffea8
piotr <pimazur@gmail.com>
2018-08-20 18:34:43 +0200

    say hallo counter

--- src/main/java/epam/workshop/controller/HiController.java
+++ src/main/java/epam/workshop/controller/HiController.java
@@ -1,14 +1,34 @@
 package epam.workshop.controller;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 public class HiController {
 
+    @Autowired
+    MeterRegistry registry;
+
+    private Counter counter;
+
+    @PostConstruct
+    public void setUp() {
+        counter = Counter
+                .builder("say.hello.hit.count")
+                .description("indicates instance count of the object")
+                .tags("dev", "performance")
+                .register(registry);
+    }
+
     @GetMapping("/sayHello")
     public ResponseEntity<String> sayHello() {
+        counter.increment();
         return ResponseEntity.ok("Hello");
     }
 }
