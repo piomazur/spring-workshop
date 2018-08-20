dcee45f922f895f2bd7325ab8f70064b6b4024b8
piotr <pimazur@gmail.com>
2018-08-20 02:15:57 +0200

    default

---
+++ src/main/java/epam/workshop/configuration/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package epam.workshop.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.StaticResourceRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        log.info("Disabling CSRF, enabling basic authentication...");
+        http
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.toAnyEndpoint())
+                .permitAll()
+                .antMatchers("/**")
+                .hasRole("USER")
+                .and()
+                .httpBasic();
+        http.csrf().disable();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password")
+                .roles("USER").build());
+        return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
